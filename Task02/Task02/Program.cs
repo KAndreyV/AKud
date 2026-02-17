14afebf9e46e6c979836353f2bb1a946e81d0e73
Андрей <rex.kudimov@mail.ru>
2026-02-17 23:55:21 +0300

    Add files via upload

---
+++ Task02/Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿StreamReader namesReader = new StreamReader("C:\\tmp\\names.txt");
+StreamWriter namesWriter = new StreamWriter("C:\\tmp\\results.txt");
+
+
+while (!namesReader.EndOfStream)
+{
+    string name = namesReader.ReadLine();
+    if (!string.IsNullOrEmpty(name) && name.Substring(0, 1) == "V")
+    {
+        namesWriter.WriteLine(name);
+        Console.WriteLine(name);
+    }
+}
+
+
+namesReader.Close();
+namesWriter.Close();

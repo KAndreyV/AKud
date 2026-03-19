d926ca7f4bb8cb6c50e1ca5fb8f5872096dc4a77
Андрей <rex.kudimov@mail.ru>
2026-03-19 23:24:26 +0300

    Add files via upload

---
+++ Task06/Task06/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace DoubleNumberApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DoubleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(NumberTextBox.Text, out int number))
+            {
+                DoubleButton.Content = "Я такое не удваиваю";
+                return;
+            }
+
+            number *= 2;
+            NumberTextBox.Text = number.ToString();
+
+            if (number > 77)
+            {
+                DoubleButton.Content = "Слишком много для меня";
+                DoubleButton.IsEnabled = false;
+            }
+            else
+            {
+                DoubleButton.Content = "Удвоить";
+            }
+        }
+    }
+}
